95324cc8c457e1e360091d92f6a0f5a42b7b7e45
hoove <windyhoo@windyhoodeMacBook-Pro.local>
2016-04-05 20:52:39 +0800

    remove log

--- pyscript/tieba/db.sql
+++ pyscript/tieba/db.sql
@@ -9,3 +9,6 @@ select (datetime('now'))
 
 insert into novel_list_dtl(dtl_id,id,title_name,content,state_date) values(1,1,'第二十五卷 神谕之战 第九十四章 王铮苏醒！','现在银盟的基本底线正在一点点消失，到时候谁都想分一杯羹的时候，我们可能就真的危险了。”木真说道，从银盟回来，木真跟一般巨人已经有了巨大的',datetime('now'));
 insert into novel_list_dtl(dtl_id,id,title_name,content,state_date) values(1,1,'tt','ttt',datetime('now');
+
+
+ALTER TABLE novel_list ADD source_type INTEGER;
